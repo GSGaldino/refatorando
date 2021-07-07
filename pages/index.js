ae6f0299ca07ae7c6c34dc6e49663506f4352969
Gabriel Soares <gabrielsoaresii@outlook.com>
2021-07-07 10:55:43 -0300

    adding> meta tags, Header background

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,21 @@ export default function Home() {
       <Head>
         <title>Refatorando - Onde as idéias saem do papel!</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Refatorando - Onde as idéias saem do papel. Sites responsivos e dinâmicos, com tecnologias de ponta." />
+        <meta name="robots" content="all" />
+        <meta name="author" content="Refatorando" />
+        <meta name="keywords" content="refatorando, tecnologia, site, criador, website, fábrica, software, programa, orçamento, página, web, react, idéias, ideias, Refatorando" />
+        <meta property="og:type" content="page" />
+        <meta property="og:url" content="https://refatorando.com.br" />
+        <meta property="og:title" content="Refatorando" />
+        <meta property="og:image" content="http://refatorando.com.br/logo.svg" />
+        <meta property="og:description" content="Onde as idéias saem do papel!" />
+        <meta property="article:author" content="Refatorando" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@" />
+        <meta name="twitter:title" content="Refatorando" />
+        <meta name="twitter:creator" content="@" />
+        <meta name="twitter:description" content="Onde as idéias saem do papel!" />
       </Head>
 
       <Header />
