777636da372c1894bdfe064e8a0dd9506e3235a9
Gabriel <60300935+GSGaldino@users.noreply.github.com>
2021-08-18 15:12:36 -0300

    fixing: global site tag

--- pages/index.js
+++ pages/index.js
@@ -34,21 +34,17 @@ export default function Home() {
         {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
+            data-cfasync="false"
             src={`https://www.googletagmanager.com/gtag/js?id=UA-204838861-1`}
           />
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              (function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-    
-              ga('create', 'UA-204838861-1', 'auto');
-              ga('send', 'pageview');
-              ga('set', 'dimension1', 'gabriel.galdino@code7.com');
+              window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-204838861-1');
             `,
             }}
           />
